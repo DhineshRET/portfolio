92fa6d86b31b7f3842ee7b5c34643257538df185
DhineshRET <dhineshrethinam@gmail.com>
2024-09-01 13:03:17 +0530

    streamlit test

--- test.py
+++ test.py
@@ -2,23 +2,6 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    #st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    #st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    ##st.write(dataframe)
 
 
 
@@ -30,7 +13,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache_data
 def load_data(nrows):
-    data = dataframe
+    data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -54,3 +37,4 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+S
