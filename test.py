aaa881c44b6bd714376662004672943b81d69caa
DhineshRET <dhineshrethinam@gmail.com>
2024-09-03 14:01:57 +0530

    read column names

--- test.py
+++ test.py
@@ -1,29 +1,44 @@
-
 import streamlit as st
-import os.path
-import pathlib
-st.write("""
-# File Picker
-""")
-uploaded_file = st.file_uploader("Choose a CSV file")
-if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    data = uploaded_file.getvalue().decode('utf-8').splitlines()         
-    st.session_state["preview"] = ''
-    for i in range(0, min(5, len(data))):
-        st.session_state["preview"] += data[i]
-preview = st.text_area("CSV Preview", "", height=150, key="preview")
-upload_state = st.text_area("Upload State", "", key="upload_state")
-def upload():
-    if uploaded_file is None:
-        st.session_state["upload_state"] = "Upload a file first!"
-    else:
-        data = uploaded_file.getvalue().decode('utf-8')
-        parent_path = pathlib.Path(__file__).parent.parent.resolve()           
-        save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)
-        destination_file = open(complete_name, "w")
-        destination_file.write(data)
-        destination_file.close()
-        st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
-st.button("Upload file to Sandbox", on_click=upload)
+import pandas as pd
+from io import StringIO
+
+st.title('Check')
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    temp=pd.read_csv(uploaded_files._file_urls)
+    st.write("filename:", uploaded_file.name,"column:",temp.column)
+    # st.write(bytes_data)
+
+# DATE_COLUMN = 'date/time'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# @st.cache_data
+# def load_data(nrows):
+#     data = pd.read_csv(DATA_URL, nrows=nrows)
+#     lowercase = lambda x: str(x).lower()
+#     data.rename(lowercase, axis='columns', inplace=True)
+#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     return data
+
+# data_load_state = st.text('Loading data...')
+# data = load_data(10000)
+# data_load_state.text("Done! (using st.cache_data)")
+
+# if st.checkbox('Show raw data'):
+#     st.subheader('Raw data')
+#     st.write(data)
+
+# st.subheader('Number of pickups by hour')
+# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+# st.bar_chart(hist_values)
+
+# # Some number in the range 0-23
+# hour_to_filter = st.slider('hour', 0, 23, 17)
+# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+# st.map(filtered_data)
