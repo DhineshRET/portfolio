f0090b530a8962c190c866b508700e9bd1eea116
DhineshRET <dhineshrethinam@gmail.com>
2024-09-03 14:14:34 +0530

    read column names

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ uploaded_files = st.file_uploader(
 )
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    #temp=pd.read_csv(uploaded_files._file_urls)
+    temp=pd.read_csv(uploaded_file)
     st.write("filename:", uploaded_file.name,"column:",uploaded_file.read)
     # st.write(bytes_data)
 
